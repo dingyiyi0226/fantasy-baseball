4d1da22a7bb10eb3fbe534e5a8cdd50cf728a8f8
Huan-Ting Chen <dingyiyi0226@gmail.com>
2022-06-09 20:37:47 +0800

    basic table

--- src/apis.js
+++ src/apis.js
@@ -31,43 +31,21 @@ const baseURL = 'https://fantasysports.yahooapis.com/fantasy/v2';
 
 const apis = {
 
-  async getMatchup(team) {
+  async getTeamStatsByWeek(team, week) {
     try {
-      const testStr = `${baseURL}/team/${CONFIG.LEAGUE_KEY}.t.${team}/matchups`;
-      const results = await makeAPIrequest(testStr);
-      return results.team;
-    } catch (err) {
-      console.error(`Error in getMatchup(): ${err}`);
-      return err;
-    }
-  },
-
-  async getTeam(team) {
-    try {
-      const testStr = `${baseURL}/team/${CONFIG.LEAGUE_KEY}.t.${team}/metadata`;
-      const results = await makeAPIrequest(testStr);
-      return results.team;
-    } catch (err) {
-      console.error(`Error in getTeam(): ${err}`);
-      return err;
-    }
-  },
-
-  async getTeamStat(team, week) {
-    try {
-      const testStr = `${baseURL}/team/${CONFIG.LEAGUE_KEY}.t.${team}/stats;type=week;week=${week}`;
-      const results = await makeAPIrequest(testStr);
+      const query = `${baseURL}/team/${CONFIG.LEAGUE_KEY}.t.${team}/stats;type=week;week=${week}`;
+      const results = await makeAPIrequest(query);
       return results.team.team_stats.stats.stat;
     } catch (err) {
-      console.error(`Error in getTeamStat(): ${err}`);
+      console.error(`Error in getTeamStatsByWeek(): ${err}`);
       return err;
     }
   },
 
   async getTeams() {
     try {
-      const testStr = `${baseURL}/league/${CONFIG.LEAGUE_KEY}/teams`;
-      const results = await makeAPIrequest(testStr);
+      const query = `${baseURL}/league/${CONFIG.LEAGUE_KEY}/teams`;
+      const results = await makeAPIrequest(query);
       return results.league.teams.team;
     } catch (err) {
       console.error(`Error in getTeams(): ${err}`);
@@ -77,21 +55,9 @@ const apis = {
 
   async getLeague() {
     try {
-      const testStr = `${baseURL}/league/${CONFIG.LEAGUE_KEY}/metadata`;
-      const results = await makeAPIrequest(testStr);
+      const query = `${baseURL}/league/${CONFIG.LEAGUE_KEY}/settings`;
+      const results = await makeAPIrequest(query);
       return results.league;
-    } catch (err) {
-      console.error(`Error in getLeague(): ${err}`);
-      return err;
-    }
-  },
-
-  async getLeagueSettings() {
-    try {
-      const testStr = `${baseURL}/league/${CONFIG.LEAGUE_KEY}/settings`;
-      const results = await makeAPIrequest(testStr);
-      console.log(results)
-      return results.league.settings;
     } catch (err) {
       console.error(`Error in getLeagueSettings(): ${err}`);
       return err;
