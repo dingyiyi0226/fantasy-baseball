bb6d71f93a7e192083beaacb8d30af54e4dc8bf9
Huan-Ting Chen <dingyiyi0226@gmail.com>
2022-06-10 14:32:09 +0800

    basic ranking

--- src/Stats.js
+++ src/Stats.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Paper from '@mui/material/Paper';
 import Select from '@mui/material/Select';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 
-
 import apis from './apis.js'
+
 
 class Stats extends Component {
   constructor(props) {
@@ -17,11 +18,14 @@ class Stats extends Component {
       fetchMetadata: true,
       fetchStats: true,
       week: 1,
+      type: 'value', // 'value', 'rank'
       stats: {},
+      ranks: {},
+
+      league: {},
       teams: [],
       teamID2Name: {},
       statID2Name: {},
-      league: {},
     }
   }
 
@@ -63,12 +67,64 @@ class Stats extends Component {
     })
   }
 
+  calulateRank = () => {
+    const statCategories = this.state.league.settings.stat_categories.stats.stat
+                               .filter(s => !s.is_only_display_stat)
+    let statsT = {}
+    statCategories.forEach(s => {
+      statsT[s.stat_id] = []
+    })
+
+    Object.values(this.state.stats).forEach(stat => {
+      stat.forEach(s => {
+        statsT[s.stat_id].push(s.value)
+      })
+    })
+
+    for (let [stat_id, stat] of Object.entries(statsT)){
+      const sort_order = statCategories.find(s => s.stat_id === Number(stat_id)).sort_order === 0;
+      stat.sort((a, b) => sort_order ? a-b : b-a)
+    }
+
+    const ranks = {}
+    this.state.teams.forEach(team => {
+      ranks[team.team_id] = []
+    })
+
+    for (let [team_id, stats] of Object.entries(this.state.stats)){
+      stats.forEach(stat => {
+        const rank = statsT[stat.stat_id].indexOf(stat.value) + 1;
+        ranks[team_id].push({stat_id: stat.stat_id, value: rank})
+      })
+    }
+    this.setState({ranks: ranks})
+  }
+
   onSelectWeek = (e) => {
+    if (this.state.fetchStats) {
+      return
+    }
     this.setState({
       week: e.target.value,
+      ranks: {},
+      type: 'value',
       fetchStats: true,
     })
     this.fetchStats()
+  }
+
+  onSelectType = (e) => {
+    if (this.state.fetchStats) {
+      return
+    }
+
+    if (Object.keys(this.state.ranks).length === 0) {
+      this.calulateRank()
+    }
+
+    this.setState({
+      type: e.target.value,
+    })
   }
 
 
@@ -88,9 +144,9 @@ class Stats extends Component {
     })
   }
 
-  componentDidMount() {
-    this.fetchMetadata()
-    this.fetchStats()
+  async componentDidMount() {
+    await this.fetchMetadata()
+    await this.fetchStats()
   }
 
   render() {
@@ -101,26 +157,34 @@ class Stats extends Component {
     return (
       <Container>
         <Grid container spacing={2}>
-          <Grid item xs={2}>
+          <Grid item xs={3}>
+            <InputLabel id="week-label">Week</InputLabel>
             <Select
-              labelId="demo-simple-select-filled-label"
-              id="demo-simple-select-filled"
+              labelId="week-label"
+              id="week-selector"
               value={this.state.week}
               onChange={this.onSelectWeek}
             >
-              <MenuItem value={this.state.week}>{
-                this.state.week}
-              </MenuItem>
               {[...Array(this.state.league.end_week-this.state.league.start_week+1).keys()].map(i => (
                 <MenuItem value={i+1}>{i+1}</MenuItem>
               ))}
 
             </Select>
           </Grid>
-          <Grid item xs={2}>
-            Stat/Rank
+          <Grid item xs={3}>
+            <InputLabel id="type-label">Type</InputLabel>
+            <Select
+              labelId="type-label"
+              id="type-selector"
+              value={this.state.type}
+              onChange={this.onSelectType}
+            >
+              <MenuItem value="value">Value</MenuItem>
+              <MenuItem value="rank">Rank</MenuItem>
+
+            </Select>
           </Grid>
-          <Grid item xs={8}>
+          <Grid item xs={6}>
           </Grid>
 
         </Grid>
@@ -145,11 +209,18 @@ class Stats extends Component {
                     <TableCell align="right" component="th" scope="row">
                       {this.state.statID2Name[statID]}
                     </TableCell>
-                    {Object.keys(this.state.stats).map((teamID) => (
+                    {this.state.type === 'value' ?
+                      Object.keys(this.state.stats).map((teamID) => (
                       <TableCell align="right">
                         {this.state.stats[teamID].find(s => s.stat_id === Number(statID)).value}
                       </TableCell>
-                    ))}
+                      )) :
+                      Object.keys(this.state.ranks).map((teamID) => (
+                      <TableCell align="right">
+                        {this.state.ranks[teamID].find(s => s.stat_id === Number(statID)).value}
+                      </TableCell>
+                      ))
+                    }
                   </TableRow>
                 ))}
               </TableBody>
