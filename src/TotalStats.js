1026e43cd8ee7cbe3671dd967ff4d71f5b95b65c
Huan-Ting Chen <dingyiyi0226@gmail.com>
2022-06-11 17:28:00 +0800

    update totalStat filter

--- src/TotalStats.js
+++ src/TotalStats.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import Paper from '@mui/material/Paper';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
 import apis from './apis.js'
 
@@ -17,8 +22,10 @@ class TotalStats extends Component {
     this.state = {
       fetching: true,
       stats: {},
+      calType: 'all', // 'all', 'batting', 'pitching'
+      sum: {},
+      rank: {},
     }
-
   }
 
   getMatchupsUntilWeek = async () => {
@@ -29,7 +36,7 @@ class TotalStats extends Component {
     })
     this.statCate.forEach(s => {
       Object.keys(allStats).forEach(key => {
-        allStats[key]['stat'] = {...allStats[key]['stat'], [s.stat_id]: {'win': 0, 'lose': 0, 'tie': 0}};
+        allStats[key][s.stat_id] = {'win': 0, 'lose': 0, 'tie': 0};
       })
     })
 
@@ -38,33 +45,81 @@ class TotalStats extends Component {
       team.matchups.matchup.forEach(week => {
         week.stat_winners.stat_winner.forEach(stat_winner => {
           if (stat_winner.is_tied) {
-            allStats[team.team_id]['stat'][stat_winner.stat_id].tie += 1;
+            allStats[team.team_id][stat_winner.stat_id].tie += 1;
           } else if (stat_winner.winner_team_key === team.team_key) {
-            allStats[team.team_id]['stat'][stat_winner.stat_id].win += 1;
+            allStats[team.team_id][stat_winner.stat_id].win += 1;
           } else {
-            allStats[team.team_id]['stat'][stat_winner.stat_id].lose += 1;
+            allStats[team.team_id][stat_winner.stat_id].lose += 1;
           }
         })
       })
     })
-    this.teams.forEach(team => {
-      const sum = Object.values(allStats[team.team_id]['stat'])
-        .reduce((pv, v) => ({'win': pv.win+v.win, 'lose': pv.lose+v.lose, 'tie': pv.tie+v.tie}), {'win': 0, 'lose': 0, 'tie': 0});
-      allStats[team.team_id]['sum'] = sum;
-    })
-
-    const point = this.teams.map(team =>
-      allStats[team.team_id]['sum'].win - allStats[team.team_id]['sum'].lose
-    ).sort((a, b) => b-a)
-
-    this.teams.forEach(team => {
-      const rank = point.indexOf(allStats[team.team_id]['sum'].win - allStats[team.team_id]['sum'].lose) + 1;
-      allStats[team.team_id]['rank'] = rank;
-    })
+    const calVal = this.calStats(this.state.calType, allStats);
 
     this.setState({
       stats: allStats,
-      fetching: false
+      sum: calVal.sum,
+      rank: calVal.rank,
+      fetching: false,
+    })
+  }
+
+  calStats = (calType, stats=null) => {
+    let sum = {};
+    let rank = {};
+
+    if (stats === null) {
+      stats = this.state.stats;
+    }
+
+    this.teams.forEach(team => {
+      if (calType === 'pitching') {
+        let result = {'win': 0, 'lose': 0, 'tie': 0};
+        for (let [stat, val] of Object.entries(stats[team.team_id])) {
+          if (this.statCate.find(s => s.stat_id === Number(stat)).position_type === 'P') {
+            result.win += val.win;
+            result.lose += val.lose;
+            result.tie += val.tie;
+          }
+        }
+        sum[team.team_id] = result;
+      }
+      else if (calType === 'batting') {
+        let result = {'win': 0, 'lose': 0, 'tie': 0};
+        for (let [stat, val] of Object.entries(stats[team.team_id])) {
+          if (this.statCate.find(s => s.stat_id === Number(stat)).position_type === 'B') {
+            result.win += val.win;
+            result.lose += val.lose;
+            result.tie += val.tie;
+          }
+        }
+        sum[team.team_id] = result;
+      }
+      else {
+        sum[team.team_id] = Object.values(stats[team.team_id])
+          .reduce((pv, v) => ({'win': pv.win+v.win, 'lose': pv.lose+v.lose, 'tie': pv.tie+v.tie}), {'win': 0, 'lose': 0, 'tie': 0});
+      }
+    })
+
+    const points = this.teams.map(team => sum[team.team_id].win - sum[team.team_id].lose)
+                            .sort((a, b) => b-a)
+    this.teams.forEach(team => {
+      rank[team.team_id] = points.indexOf(sum[team.team_id].win - sum[team.team_id].lose) + 1;
+    })
+
+    return {'sum': sum, 'rank': rank}
+  }
+
+  onSelectType = (e) => {
+    if (this.state.fetching) {
+      return
+    }
+
+    const calVal = this.calStats(e.target.value);
+    this.setState({
+      calType: e.target.value,
+      sum: calVal.sum,
+      rank: calVal.rank,
     })
   }
 
@@ -73,60 +128,92 @@ class TotalStats extends Component {
   }
 
   render() {
-    return (this.state.fetching ?
-      <h3 className="fetching-text">Fetching</h3> :
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} size="small" aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell> </TableCell>
-              {this.teams.map((team) =>
-                <TableCell align="right">{team.name}</TableCell>
-              )}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.statCate.map((stat) => (
-              <TableRow
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell align="right" component="th" scope="row">
-                  {stat.display_name}
-                </TableCell>
-                {Object.keys(this.state.stats).map((teamID) => {
-                  const s = this.state.stats[teamID]['stat'][stat.stat_id]
-                  return <TableCell align="right">{`${s.win}-${s.lose}-${s.tie}`}</TableCell>
-                })}
-              </TableRow>
-            ))}
-            <TableRow
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+    return (
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={3}>
+            <InputLabel id="type-label">Type</InputLabel>
+            <Select
+              labelId="type-label"
+              id="type-selector"
+              value={this.state.calType}
+              onChange={this.onSelectType}
             >
-              <TableCell align="right" component="th" scope="row">
-                Sum
-              </TableCell>
-              {this.teams.map(team =>
-                <TableCell align="right">
-                  {`${this.state.stats[team.team_id].sum.win}-${this.state.stats[team.team_id].sum.lose}-${this.state.stats[team.team_id].sum.tie}`}
-                </TableCell>
-              )}
-            </TableRow>
-            <TableRow
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell align="right" component="th" scope="row">
-                Rank
-              </TableCell>
-              {this.teams.map(team =>
-                <TableCell align="right">
-                  {this.state.stats[team.team_id].rank}
-                </TableCell>
-              )}
-            </TableRow>
-            
-          </TableBody>
-        </Table>
-      </TableContainer>
+              <MenuItem value="all">All</MenuItem>
+              <MenuItem value="batting">Batting</MenuItem>
+              <MenuItem value="pitching">Pitching</MenuItem>
+
+            </Select>
+          </Grid>
+          
+          <Grid item xs={9}>
+          </Grid>
+
+        </Grid>
+
+        {this.state.fetching ?
+          <h3 className="fetching-text">Fetching</h3> :
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} size="small" aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell> </TableCell>
+                  {this.teams.map((team) =>
+                    <TableCell align="right">{team.name}</TableCell>
+                  )}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.statCate.filter(stat => {
+                  if (this.state.calType === 'pitching'){
+                    return stat.position_type === 'P'
+                  } else if (this.state.calType === 'batting') {
+                    return stat.position_type === 'B'
+                  } else {
+                    return true
+                  }}).map((stat) => (
+                  <TableRow
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell align="right" component="th" scope="row">
+                      {stat.display_name}
+                    </TableCell>
+                    {Object.keys(this.state.stats).map((teamID) => {
+                      const s = this.state.stats[teamID][stat.stat_id]
+                      return <TableCell align="right">{`${s.win}-${s.lose}-${s.tie}`}</TableCell>
+                    })}
+                  </TableRow>
+                ))}
+                <TableRow
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell align="right" component="th" scope="row">
+                    Sum
+                  </TableCell>
+                  {this.teams.map(team =>
+                    <TableCell align="right">
+                      {`${this.state.sum[team.team_id].win}-${this.state.sum[team.team_id].lose}-${this.state.sum[team.team_id].tie}`}
+                    </TableCell>
+                  )}
+                </TableRow>
+                <TableRow
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell align="right" component="th" scope="row">
+                    Rank
+                  </TableCell>
+                  {this.teams.map(team =>
+                    <TableCell align="right">
+                      {this.state.rank[team.team_id]}
+                    </TableCell>
+                  )}
+                </TableRow>
+
+              </TableBody>
+            </Table>
+          </TableContainer>
+        }
+      </Container>
     )
   }
 }
