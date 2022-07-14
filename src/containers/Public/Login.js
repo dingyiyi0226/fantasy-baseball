90ff4d35b70f76579dc73c0273ad062e88a57b8f
Huan-Ting Chen <dingyiyi0226@gmail.com>
2022-07-15 02:55:13 +0800

    modify login page

--- src/containers/Public/Login.js
+++ src/containers/Public/Login.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
 import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 const oauth_link = `https://api.login.yahoo.com/oauth2/request_auth?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=code`;
 
@@ -10,10 +12,19 @@ const oauth_link = `https://api.login.yahoo.com/oauth2/request_auth?client_id=${
 function Login(props) {
   return (
     <Container>
-      <Toolbar/>
-      <a href={oauth_link}>
-        <Box component="img" sx={{ width: 250 }} alt="yahoo-login" src={process.env.PUBLIC_URL+'/yahoo_login.png'}/>
-      </a>
+      <Toolbar variant="dense"/>
+      <Box sx={{p: 3}}>
+        <Stack direction="column" spacing={4} alignItems="center" justifyContent="center">
+          <Typography variant="h3" align="center" sx={{fontWeight: 'bold'}}>
+            Fantasy Baseball Helper
+          </Typography>
+
+          <a href={oauth_link}>
+            <Box component="img" sx={{ width: 250 }} alt="yahoo-login" src={process.env.PUBLIC_URL+'/yahoo_login.png'}/>
+          </a>
+          
+        </Stack>
+      </Box>
     </Container>
   )
 }
