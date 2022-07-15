6400f05adbe663c8b47475263b491767623af09b
Huan-Ting Chen <dingyiyi0226@gmail.com>
2022-07-16 01:54:05 +0800

    update matchups UI

--- src/containers/Teams/TeamMatchups.js
+++ src/containers/Teams/TeamMatchups.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import Container from '@mui/material/Container';
 import FormControl from '@mui/material/FormControl';
+import Grid from '@mui/material/Grid';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Paper from '@mui/material/Paper';
@@ -86,6 +87,42 @@ function TeamMatchups(props) {
     navigate(`/teams/${e.target.value}/matchups`);
   }
 
+  const matchupsTable = (matchups) => (
+    <TableContainer component={Paper}>
+      <Table size="small" sx={{minWidth: 300, 'th': {fontWeight: 'bold'}}} aria-label="matchup-table">
+        <TableHead>
+          <TableRow>
+            <TableCell sx={{minWidth: 80, maxWidth: 90}}> </TableCell>
+            <TableCell align="right" sx={{minWidth: 70}}>Opponent</TableCell>
+            <TableCell align="center" sx={{minWidth: 60, maxWidth: 90}}>Status</TableCell>
+            <TableCell align="center" sx={{minWidth: 70, maxWidth: 100}}>W-L-T</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {matchups.map(matchup => (
+            <TableRow
+              key={matchup.week}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="right" component="th" scope="row">
+                {`Week ${matchup.week}`}
+              </TableCell>
+              <TableCell align="right">
+                {getOpponent(matchup, 'name')}
+              </TableCell>
+              <TableCell align="center">
+                {getStatus(matchup)}
+              </TableCell>
+              <TableCell align="center">
+                {getScore(matchup, true)}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+
   return (
     <Container>
       <Stack direction="row" spacing={2} alignItems="center" justifyContent="flex-start">
@@ -106,39 +143,26 @@ function TeamMatchups(props) {
 
       {fetching ?
         <FetchingText /> :
-        <TableContainer component={Paper} sx={{ my: 2, width: '70%' }}>
-          <Table size="small" sx={{minWidth: 300, 'th': {fontWeight: 'bold'}}} aria-label="matchup-table">
-            <TableHead>
-              <TableRow>
-                <TableCell sx={{minWidth: 80, maxWidth: 90}}> </TableCell>
-                <TableCell align="right" sx={{minWidth: 70}}>Opponent</TableCell>
-                <TableCell align="right" sx={{minWidth: 60, maxWidth: 90}}>Status</TableCell>
-                <TableCell align="right" sx={{minWidth: 70, maxWidth: 100}}>W-L-T</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {matchups.map(matchup => (
-                <TableRow
-                  key={matchup.week}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell align="right" component="th" scope="row">
-                    {`Week ${matchup.week}`}
-                  </TableCell>
-                  <TableCell align="right">
-                    {getOpponent(matchup, 'name')}
-                  </TableCell>
-                  <TableCell align="right">
-                    {getStatus(matchup)}
-                  </TableCell>
-                  <TableCell align="right">
-                    {getScore(matchup, true)}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <Grid container spacing={2} justifyContent="center" sx={{py: 2}}>
+          <Grid item xs={12} sm={10} md={8} sx={{display: {lg: 'none'}}}>
+            {matchupsTable(matchups)}
+          </Grid>
+          {matchups.length > 13 ? (
+            <React.Fragment>
+              <Grid item lg={6} sx={{display: {xs: 'none', lg: 'block'}}}>
+                {matchupsTable(matchups.slice(0, 13))}
+              </Grid>
+              <Grid item lg={6} sx={{display: {xs: 'none', lg: 'block'}}}>
+                {matchupsTable(matchups.slice(13))}
+              </Grid>
+            </React.Fragment>
+          ) : (
+            <Grid item lg={6} sx={{display: {xs: 'none', lg: 'block'}}}>
+              {matchupsTable(matchups)}
+            </Grid>
+          )}
+          
+        </Grid>
       }
     </Container>
   )
